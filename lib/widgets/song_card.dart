86de58c1549eff20f028a952d860391edf711850
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-10-20 17:12:32 +0800

    Horizontal scrolling (full screen cards)

--- lib/widgets/song_card.dart
+++ lib/widgets/song_card.dart
@@ -1,82 +1,86 @@
 import 'package:flutter/material.dart';
 
-class SongCard extends StatelessWidget {
+class SongCard extends StatefulWidget {
   final String title;
-  final playButtonSize = 50.0;
 
   SongCard(this.title);
 
   @override
+  _SongCardState createState() => _SongCardState();
+}
+
+class _SongCardState extends State<SongCard> {
+  var _playButtonSize = 80.0;
+  var _icon = Icons.play_arrow;
+
+  onCardTap() {
+    setState(() {
+      if (_icon == Icons.play_arrow) {
+        _icon = Icons.pause;
+      } else {
+        _icon = Icons.play_arrow;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
+    final mediaQuery = MediaQuery.of(context);
+
+    return GestureDetector(
+      onTap: () => onCardTap(),
+      child: Card(
+          child: SizedBox(
+        height: mediaQuery.size.height,
+        width: mediaQuery.size.width,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              "assets/images/ses.jpg",
+              fit: BoxFit.fitHeight,
+              height: mediaQuery.size.height,
+            ),
+            // Song Title Banner
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                ),
+                height: 90,
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: Text(
+                  widget.title,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ),
+            // Play Button
+            Padding(
+              padding: EdgeInsets.all(45),
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 900),
+                height: _playButtonSize,
+                width: _playButtonSize,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white.withOpacity(0.2),
+                ),
+                alignment: Alignment.center,
+                child: Icon(
+                  _icon,
+                  size: 50.0,
+                  color: Colors.black38,
+                ),
+              ),
+            ),
+          ],
         ),
-        // color: Colors.transparent,
-        elevation: 10,
-        child: SizedBox(
-          height: 250,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(
-                    Colors.grey,
-                    BlendMode.modulate,
-                  ),
-                  child: Image.asset("assets/images/ses.jpg",
-                      fit: BoxFit.fitWidth,
-                      width:
-                          350 // TODO: Make responsive, probably not good to hard code image width
-                      ),
-                ),
-              ),
-              // The song title banner slides off in the hero animation.
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.4),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15.0),
-                      bottomRight: Radius.circular(15.0),
-                    ),
-                  ),
-                  height: 60,
-                  // color: Colors.black12,
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.symmetric(horizontal: 12),
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 21,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              // The play button grows in the hero animation.
-              Padding(
-                padding: EdgeInsets.all(45),
-                child: Container(
-                  height: playButtonSize,
-                  width: playButtonSize,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white.withOpacity(0.2),
-                  ),
-                  alignment: Alignment.center,
-                  child: Icon(Icons.play_arrow,
-                      size: playButtonSize, color: Colors.black38),
-                ),
-              ),
-            ],
-          ),
-        ));
+      )),
+    );
   }
 }
